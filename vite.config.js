1b024df998443e09353f0798bcb8fa89f53dd837
JMuff22 <jake.muff@vtt.fi>
2025-03-07 14:52:56 +0200

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/resource-estimator/', // Repo path name for GitHub Pages
+  base: '', // Repo path name for GitHub Pages
   
   build: {
     outDir: 'dist',
