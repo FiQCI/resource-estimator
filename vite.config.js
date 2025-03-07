dac2002d31a7347d7977c9d73199ff36eaa4679b
JMuff22 <jake.muff@vtt.fi>
2025-03-07 13:46:29 +0200

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	base: '/resource-estimator/', // Repo path name
+	
+	// Configure the build output
+	build: {
+		outDir: 'dist',
+		emptyOutDir: true,
+	}
 })
